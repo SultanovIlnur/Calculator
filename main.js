eebc5e2c88b2d2e99ef7d1c4905ce36b156dbbdf
SultanovIlnur <68385484+SultanovIlnur@users.noreply.github.com>
2021-04-07 21:13:00 +0300

    added clear functionality (fix)

--- main.js
+++ main.js
@@ -2,47 +2,52 @@ var num1 = 0;
 var num2 = 0;
 var currentNumber = 1;
 var operationType = "+";
-function calculate(pressedSymbol){
-    currentNumber = 2;
-    num1 = parseInt(document.getElementById("textInput").value);
-    document.getElementById("textInput").value = "";
-    operationType = pressedSymbol;
+function calculate(pressedSymbol) {
+  currentNumber = 2;
+  num1 = parseInt(document.getElementById("textInput").value);
+  document.getElementById("textInput").value = "";
+  operationType = pressedSymbol;
 }
-function equal(){
-    currentNumber = 1;
-    num2 = parseInt(document.getElementById("textInput").value);
-    var math_op = {
-        '/': function (x, y) { return x / y },
-        '*': function (x, y) { return x * y },
-        '+': function (x, y) { return x + y },
-        '-': function (x, y) { return x - y }
-        
-    }
-    let result = math_op[operationType](num1, num2);
-    if (result.length > 8){
-        document.getElementById("textInput").value = "ERR";
-    }
-    else{
-        document.getElementById("textInput").value = result;
-    }
-    
+function equal() {
+  currentNumber = 1;
+  num2 = parseInt(document.getElementById("textInput").value);
+  var math_op = {
+    "/": function (x, y) {
+      return x / y;
+    },
+    "*": function (x, y) {
+      return x * y;
+    },
+    "+": function (x, y) {
+      return x + y;
+    },
+    "-": function (x, y) {
+      return x - y;
+    },
+  };
+  let result = math_op[operationType](num1, num2);
+  if (result.length > 8) {
+    document.getElementById("textInput").value = "ERR";
+  } else {
+    document.getElementById("textInput").value = result;
+  }
 }
-function clear(){
-if (currentNumber == 1){
+function clear() {
+  if (currentNumber == 1) {
     num1 = 0;
-}
-else{
-num2 = 0;
-}
-}
-function clearAll(){
-    num1 = 0;
+  } else {
     num2 = 0;
-    document.getElementById("textInput").value = "";
+  }
+  document.getElementById("textInput").value = "";
 }
-function digit(pressedDigit){
-    if (document.getElementById("textInput").value.length <=8){
-        document.getElementById("textInput").value += pressedDigit;
-    }
-    
+
+function clearAll() {
+  num1 = 0;
+  num2 = 0;
+  document.getElementById("textInput").value = "";
+}
+function digit(pressedDigit) {
+  if (document.getElementById("textInput").value.length <= 8) {
+    document.getElementById("textInput").value += pressedDigit;
+  }
 }
